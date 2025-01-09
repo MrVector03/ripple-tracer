833db89fc299748a18a7256ff75853d48e619440
Viktor <viktormilojevic.03@gmail.com>
2025-01-09 06:50:09 +0100

    Attempt 3 v2 lowkey closer

--- res/shaders/custom_water_shader_v1/vert.glsl
+++ res/shaders/custom_water_shader_v1/vert.glsl
@@ -1,18 +1,17 @@
-#version 410 core
+#version 330
 
 layout (location = 0) in vec3 position;
+layout (location = 1) in vec2 uv;
 
-uniform mat4 uni_VP;
+out vec2 frag_uv;
+out vec3 frag_position;
+
 uniform mat4 uni_M;
-
-out vec3 fragColor;
-
-uniform vec4 plane;
+uniform mat4 uni_VP;
 
 void main()
 {
+    frag_uv = uv;
+    frag_position = position;
     gl_Position = uni_VP * uni_M * vec4(position, 1.0);
-    fragColor = vec3(0.0, 0.0, 1.0);
-
-    gl_ClipDistance[0] = dot(gl_Position, plane);
 }
